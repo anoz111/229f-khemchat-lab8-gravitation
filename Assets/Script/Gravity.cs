42a12debf2f08217c283597d7c192c08a48211a9
anoz111 <khamchat2548@gmail.com>
2025-03-13 16:24:32 +0700

    Use Gravity

---
+++ Assets/Script/Gravity.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Gravity : MonoBehaviour
+{
+    Rigidbody rb;
+    const float G = 0.00667f;
+    public static List<Gravity> gravityObjectList;
+
+    [SerializeField] bool planet = false;
+    [SerializeField] int orbitSpeed = 1000;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if(gravityObjectList == null) 
+        {
+            gravityObjectList = new List<Gravity>();
+        }
+        gravityObjectList.Add(this);
+
+        if (!planet) 
+        {
+            rb.AddForce(Vector3.left * orbitSpeed);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var obj in gravityObjectList) 
+        {
+            if (obj != this)
+            {
+                Attract(obj);
+            }
+
+        }
+    }
+
+    void Attract(Gravity other)
+    {
+        Rigidbody otherRb = other.rb;
+        Vector3 direction = rb.position - otherRb.position;
+        float distance = direction.magnitude;
+
+        float forceMagnitude = G * (rb.mass * otherRb.mass/ Mathf.Pow(distance,2));
+        Vector3 finalGF = forceMagnitude * direction.normalized;
+
+        otherRb.AddForce(finalGF);
+    }
+}
